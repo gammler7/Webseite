d00b3512e189bb419201b0532a60cb9ff1a25e1f
gammler7 <85961550+gammler7@users.noreply.github.com>
2026-03-02 14:57:46 +0100

    Ostereier statt Confetti

--- script.js
+++ script.js
@@ -12,29 +12,39 @@
   }
 
   function createParticle() {
+    var base = 14 + Math.random() * 12;
     return {
       x: Math.random() * canvas.width,
       y: -10,
-      w: 6 + Math.random() * 8,
-      h: 4 + Math.random() * 6,
+      w: base,
+      h: base * 1.35, // etwas länglicher für Ei-Form
       color: colors[Math.floor(Math.random() * colors.length)],
-      vx: (Math.random() - 0.5) * 0.8,
-      vy: 1.2 + Math.random() * 1.5,
+      vx: (Math.random() - 0.5) * 0.4,
+      vy: 1.1 + Math.random() * 1.4,
       rot: Math.random() * 360,
-      rotSpeed: (Math.random() - 0.5) * 8
+      rotSpeed: (Math.random() - 0.5) * 3
     };
+  }
+
+  function drawEgg(p) {
+    ctx.save();
+    // In die Mitte des Eis verschieben
+    ctx.translate(p.x + p.w / 2, p.y + p.h / 2);
+    ctx.rotate((p.rot * Math.PI) / 180);
+    // Ei-Form: unten etwas breiter als oben
+    ctx.scale(1, 1.15);
+    ctx.beginPath();
+    ctx.ellipse(0, 0, p.w / 2, p.h / 2, 0, 0, Math.PI * 2);
+    ctx.fillStyle = p.color;
+    ctx.fill();
+    ctx.restore();
   }
 
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (var i = particles.length - 1; i >= 0; i--) {
       var p = particles[i];
-      ctx.save();
-      ctx.translate(p.x + p.w / 2, p.y + p.h / 2);
-      ctx.rotate((p.rot * Math.PI) / 180);
-      ctx.fillStyle = p.color;
-      ctx.fillRect(-p.w / 2, -p.h / 2, p.w, p.h);
-      ctx.restore();
+      drawEgg(p);
       p.x += p.vx;
       p.y += p.vy;
       p.rot += p.rotSpeed;
